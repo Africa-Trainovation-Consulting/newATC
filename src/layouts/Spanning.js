a2c6bbb9df77e9ae286433cd737255a18455c8a2
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-11-26 10:39:57 +0100

    chooseplan and upload done and dusted.

---
+++ src/layouts/Spanning.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function Spanning(props){
+    return(
+        <span className='spanning'>
+        <span className="cancel_btn">
+            <h1>Choose a Plan</h1>
+            <span className='Madbtn'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 39 39" fill="none">
+                    <path d="M13 13L19.5 19.5M26 26L19.5 19.5M19.5 19.5L26 13L13 26" stroke="#AAAAAA" />
+                    <circle cx="19.5" cy="19.5" r="19" stroke="#AAAAAA" />
+                </svg>
+            </span>
+        </span>
+    </span>
+    )
+}
+export default Spanning;
