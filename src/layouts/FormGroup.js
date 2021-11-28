2b05fd191e305a790d8399cf015c00c1925ad23b
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-11-28 21:21:17 +0100

    done for the night

--- src/layouts/FormGroup.js
+++ src/layouts/FormGroup.js
@@ -2,7 +2,7 @@ function Formgroup(props) {
     return (
         <span className="form-group">
             <label htmlFor={props.for}> {props.label} </label>
-            <input type={props.type} name={props.name} placeholder={props.placeholder} />
+            <input type={props.type} name={props.name} placeholder={props.placeholder} required />
         </span>
     )
 }
