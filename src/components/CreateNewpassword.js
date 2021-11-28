33aea27157b2997bea77b7b667c91839b30c2b2c
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-11-28 19:36:52 +0100

    createtnewpassword  modal done dusted.

--- src/components/CreateNewpassword.js
+++ src/components/CreateNewpassword.js
@@ -1,22 +1,18 @@
 import React from "react";
 import Backdrop from './HOC'
 import { Link } from 'react-router-dom'
+import Formgroup from '../layouts/FormGroup';
 function CreateNewpassword(props) {
     function continueHandler(event) {
         event.stopPropagation();
         event.preventDefault();
     }
     return (<>
-        <span className='flex_btn'>
-            <h1 style={{ textAlign: 'center' }}>
+            <h1 className='forgot'>
                 CREATE NEW PASSWORD
             </h1>
-        </span>
         <form action='post'>
-            <span className="form-group">
-                <label for="Password"> Confirm new password </label>
-                <input type="Password" name="confirmPassword" placeholder="Confirm Password" />
-            </span>
+            <Formgroup label='Confirm new Password' htmlFor='password' type='password' name='confirmPassword' placeholder='Confirm password' />
             <Link to='/passwordChanged'>
                 <button type="submit" onClick={continueHandler}> Continue <svg xmlns="http://www.w3.org/2000/svg" width="27" height="10" viewBox="0 0 37 20" fill="none">
                     <path d="M35.5 10H2M35.5 10L27.5 18.5M35.5 10L27.5 2" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
