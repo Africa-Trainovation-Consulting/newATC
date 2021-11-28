df3b42d16bc5d52e433e3188d4a96ea571b495f9
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-11-28 19:31:34 +0100

    forgotpassword  modal done dusted.

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Backdrop from './HOC'
 import { Link } from 'react-router-dom'
+import Formgroup from '../layouts/FormGroup';
 function ForgotPassword(props) {
     function continueHandler(event) {
         event.stopPropagation();
@@ -8,16 +9,11 @@ function ForgotPassword(props) {
     }
     return (
         <>
-            <span className='flex_btn'>
-                <h1>
-                    {props.title}
+                <h1 className='forgot'>
+                    FORGOT PASSWORD
                 </h1>
-            </span>
             <form action='post'>
-                <span className="form-group">
-                    <label for="Email"> {props.label} </label>
-                    <input type="email" name="email" placeholder="Enter your email" />
-                </span>
+            <Formgroup label='Email' htmlFor='Email' type='email' name='email' placeholder='Enter your email' />
                 <Link to='/createNewPassword'>
                     <button type="submit" onClick={continueHandler}> Continue <svg xmlns="http://www.w3.org/2000/svg" width="27" height="10" viewBox="0 0 37 20" fill="none">
                         <path d="M35.5 10H2M35.5 10L27.5 18.5M35.5 10L27.5 2" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
