12a35a1206f3f2613e4a8f5c3da4407952e9f989
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2021-12-02 19:00:22 +0100

    adjustment

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,8 @@ import ContinueBtn from "../layouts/ContinueBtn";
 function Login(props) {
     return (
         <>
+            <Link to='/forgotpassword' style={{ color: '#262B33', marginTop: '1em' }}>Don’t have an account?</Link>
+
             <span className="flex_btn">
                 <button className="login active">
                     Login to continue
@@ -14,23 +16,18 @@ function Login(props) {
                     Sign up to continue
                 </button>
             </span>
-            <span className="flex_btn continueWith">
-                <p className="goggle">
-                Login with <span style={{ color: 'blue', fontSize:'18px' }}>G</span><span style={{ color: 'red', fontSize:'18px' }}>o</span><span style={{ color: 'yellow',fontSize:'18px' }}>o</span><span style={{ color: 'blue', fontSize:'18px' }}>g</span><span style={{ color: 'green',fontSize:'18px' }}>l</span><span style={{ color: 'red', fontSize:'18px' }}>e </span></p>
-                <p className="facebook " >
-                Login with <span style={{ color: 'blue', fontSize:'18px' }}> Facebook </span>
-                </p>
-            </span>
+
             <div className='Or'>
-                <svg className='empty' xmlns="http://www.w3.org/2000/svg" width="259" height="2" viewBox="0 0 259 2" fill="none">
-                    <path d="M0 1L259 1.00002" stroke="#B3B3B3" stroke-width="2" />
-                </svg> OR <svg className='empty' xmlns="http://www.w3.org/2000/svg" width="259" height="2" viewBox="0 0 259 2" fill="none">
-                    <path d="M0 1L259 1.00002" stroke="#B3B3B3" stroke-width="2" />
-                </svg>
+                <p className="goggle">
+                    Login with <span style={{ color: 'blue', fontSize: '18px' }}>G</span><span style={{ color: 'red', fontSize: '18px' }}>o</span><span style={{ color: 'yellow', fontSize: '18px' }}>o</span><span style={{ color: 'blue', fontSize: '18px' }}>g</span><span style={{ color: 'green', fontSize: '18px' }}>l</span><span style={{ color: 'red', fontSize: '18px' }}>e </span>
+                </p>
+                OR <p className="facebook " >
+                    <span style={{ color: 'blue', fontSize: '18px' }}> Facebook </span>
+                </p>
             </div>
             <form action="post">
-                <Formgroup label='Email' htmlFor='Email' type='email' name='email' placeholder='Enter your email'/>
-                <Formgroup label='Password' htmlFor='password' type='password' name='password' placeholder='Enter password'/>
+                <Formgroup label='Email' htmlFor='Email' type='email' name='email' placeholder='Enter your email' />
+                <Formgroup label='Password' htmlFor='password' type='password' name='password' placeholder='Enter password' />
                 <ContinueBtn to='signup' />
             </form>
             <Link to='/forgotpassword' style={{ color: '#262B33', marginTop: '1em' }}>Forgot Password?</Link>
