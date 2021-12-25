61a78e3f4fa6b1c098140fa4e591e79e1e5b8fce
Brotherbond <ehinmitankehinde@gmail.com>
2021-12-25 22:59:19 +0100

    footer scrolling added

---
+++ assets/js/footer.js
@@ -0,0 +1,9 @@
+linkSets = document.querySelectorAll("footer .links span");
+linkSets.forEach((linkSet) => {
+  linkSet.addEventListener("click", () => {
+      linkSet.closest('div').querySelectorAll("a").forEach((a) => {
+      a.classList.toggle("none");
+    });
+    linkSet.scrollIntoViewIfNeeded()
+  });
+});
