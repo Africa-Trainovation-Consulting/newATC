7946addf0d0e2b2abd61a2fe14a3f7e0d8facf61
Brotherbond <ehinmitankehinde@gmail.com>
2021-12-30 13:27:39 +0100

    footer links svg onclick toggle added

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,7 @@
 linkSets = document.querySelectorAll("footer .links span");
 linkSets.forEach((linkSet) => {
   linkSet.addEventListener("click", () => {
+    linkSet.querySelector('svg').classList.toggle('rotate')
       linkSet.closest('div').querySelectorAll("a").forEach((a) => {
       a.classList.toggle("none_m");
     });
