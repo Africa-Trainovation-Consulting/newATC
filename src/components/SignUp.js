b2c3e7a82cf8b27b60a473f155412af0776552ad
Ade2002 <77970450+Ade2002@users.noreply.github.com>
2022-01-04 11:49:56 +0100

    validating the signup page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 import Backdrop from './HOC'
 import Formgroup from '../layouts/FormGroup';
 import ContinueBtn from "../layouts/ContinueBtn";
+import useForm from '../useForm';
+import validate from '../validateInfo';
 function SignUp(props) {
+    
+    useEffect(() => { document.title = 'SignUp' })
+    const {handleChange, values, submit, errors} = useForm(validate);
     return (
         <>
             <span className='already_'>
@@ -21,14 +27,20 @@ function SignUp(props) {
                     </Link>
                 </button>
             </span>
-            <form action="post">
+            <form action="post" name="form" method="post" validate="true" autoComplete="on" onSubmit={submit}>
                 <h3 className='title'>SIGN UP</h3>
-                <Formgroup label='Full Name' htmlFor='name' minLength='10' maxLength="60" id='name' type='text' name='fullname' placeholder='Enter your full name'  required/>
-                <Formgroup label='Email' htmlFor='email' maxLength="50" id='email' type='email' name='email' placeholder='Enter your email'  required/>
-                <Formgroup label='Home Address' htmlFor='address' id='address' type='address' name='address' placeholder='Enter your home address' />
-                <Formgroup label='Phone Number' htmlFor='Phone' id='Phone' minLength='11' maxLength="15"  type='tel' name='phonenumber' placeholder='Enter phone number'  required/>
-                <Formgroup label='Password' id='password'  minLength='8' htmlFor='password' type='password' name='password' placeholder='Enter password' required/>
-                <Formgroup label='Confirm Password'  id='confirmPassword' minLength='8' htmlFor='confirmPassword' type='password' name='confirmPassword' required placeholder='Confirm password' />
+                <Formgroup value={values.fullname} onChange={handleChange} label='Full Name' htmlFor='name' minLength='10' maxLength="60" id='name' type='text' name='fullname' placeholder='Enter your full name'  required/>
+                {errors.fullname && <i>{errors.fullname}</i>}
+                <Formgroup value={values.email} onChange={handleChange} label='Email' htmlFor='email' maxLength="50" id='email' type='email' name='email' placeholder='Enter your email'  required/>
+                {errors.email && <i>{errors.email}</i>}
+                <Formgroup value={values.address} onChange={handleChange} label='Home Address' htmlFor='address' id='address' type='address' name='address' placeholder='Enter your home address' />
+                {errors.address && <i>{errors.address}</i>}
+                <Formgroup value={values.phonenumber} onChange={handleChange} label='Phone Number' htmlFor='Phone' id='Phone' minLength='11' maxLength="15"  type='tel' name='phonenumber' placeholder='Enter phone number'  required/>
+                {errors.phonenumber && <i>{errors.phonenumber}</i>}
+                <Formgroup value={values.password} onChange={handleChange} label='Password' id='password'  minLength='8' htmlFor='password' type='password' name='password' placeholder='Enter password' required/>
+                {errors.password && <i>{errors.password}</i>}
+                <Formgroup value={values.confirmPassword} onChange={handleChange} label='Confirm Password'  id='confirmPassword' minLength='8' htmlFor='confirmPassword' type='password' name='confirmPassword' required placeholder='Confirm password' />
+                {errors.confirmPassword && <i>{errors.confirmPassword}</i>}
                 <ContinueBtn to='/upload' />
             </form>
         </>
--- src/validateInfo.js
+++ src/validateInfo.js
@@ -1,20 +1,40 @@
-export default function validateInfo(values){
+export default function validateInfo(values) {
     let errors = {}
 
+    //Full Name
+    if (!values.fullname) {
+        errors.fullname = 'Please enter your Full Name'
+    } else if (values.fullname.length < 18) {
+        errors.fullname = 'Full Name is too short, Must contain At least 15 English ALphabet'
+    }
+    //Home Address
+    if (!values.address) {
+        errors.address = 'Please Enter Your Residence Address'
+    }
+    //Phone Number
+    if (!values.phonenumber) {
+        errors.phonenumber = 'your Mobile Number Is Required'
+    }
 
     //Email
-    if(!values.email){
+    if (!values.email) {
         errors.email = 'Email is required'
-    } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
         errors.email = 'Email address is invalid'
     }
 
     //Password
-    if(!values.password){
+    if (!values.password) {
         errors.password = 'Password is required'
-    }else if (values.password.length < 8){
-        errors.password = 'password is too short'
+    } else if (values.password.length < 8) {
+        errors.password = 'Password is too short, Must contain At least 8 characters'
     }
 
-return errors;
+    //Confirm Password
+    if(!values.confirmPassword){
+        errors.confirmPassword = 'Please Confirm your password'
+    }else if (values.confirmPassword !== values.password) {
+        errors.confirmPassword = 'Passwords do not match, please check and try again.'
+    }
+    return errors;
 }
