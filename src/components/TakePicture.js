884f84dcaba56ab5bd2652da3ea04f1f2d2af342
Ademola <sikirusulaimon2002@gmail.com>
2022-01-15 13:05:41 +0100

    webcam getting the canvas image data url

--- src/components/TakePicture.js
+++ src/components/TakePicture.js
@@ -9,61 +9,63 @@ function Takepicture() {
     const [hasPhoto, setHasPhoto] = useState(false);
     const getVideo = () => {
         navigator.mediaDevices
-        .getUserMedia ({ 
-            video: {width: 1920, height: 1080}})
+            .getUserMedia({
+                video: { width: 1920, height: 1080 }
+            })
             .then(stream => {
-                let video = videoRef.current 
-                video.srcObject = stream; 
+                let video = videoRef.current
+                video.srcObject = stream;
                 video.play()
             })
             .catch(err => {
                 console.error(err)
             })
-    } 
+    }
     const takePhoto = () => {
-        let camera =  document.getElementsByClassName('camera')[0]
+        let camera = document.getElementsByClassName('camera')[0]
         camera.style.display = 'none'
         const width = 414;
-const height = width / (16 / 9)
-let video = videoRef.current;
-let photo = photoRef.current;
-photo.width = width 
-photo.height = height
-let ctx = photo.getContext('2d')
-ctx.drawImage(video, 0, 0, width, height);
-setHasPhoto(true)
-   }
+        const height = width / (16 / 9)
+        let video = videoRef.current;
+        let photo = photoRef.current;
+        photo.width = width
+        photo.height = height
+        let ctx = photo.getContext('2d')
+        ctx.drawImage(video, 0, 0, width, height);
+        setHasPhoto(true)
+       let image = document.getElementById('output')
+        let pictureURL = photo.toDataURL()
+        image.src = pictureURL
+        console.log(pictureURL)
+    }
 
 
-const closePhoto = () => {
-    let camera =  document.getElementsByClassName('camera')[0]
+    const closePhoto = () => {
+        let camera = document.getElementsByClassName('camera')[0]
         camera.style.display = 'flex'
-    let photo = photoRef.current;
-    let ctx = photo.getContext('2d')
-ctx.clearRect(0, 0, photo.width, photo.height)
-
-    setHasPhoto(false)
-}
+        let photo = photoRef.current;
+        let ctx = photo.getContext('2d')
+        ctx.clearRect(0, 0, photo.width, photo.height)
+        setHasPhoto(false)
+    }
     useEffect(() => {
         getVideo()
     }, [videoRef])
     return (
         <>
-        <Spanning heading='Upload Picture' />
-            
+            <Spanning heading='Upload Picture' />
+
             <form action="post">
-            <label className='imageUpload regular' htmlFor='myphoto'  >
-                    <input type='file' name='myphoto' /* onChange={loadfile} */ accept="image/*" className='inputImage' />
-                    <img id='output' alt='myImage' />
-                </label>
-            <span className="camera">
-                <video ref={videoRef}></video>
-                <input type='button' value='Take picture' className="takePicture regular" onClick={takePhoto} />
-            </span>
-            <span className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
-                <canvas ref={photoRef}></canvas>
-                <input type='button' value='Retake picture' className="takePicture regular" onClick={closePhoto} />
-            </span>
+                        
+                    <img id='output' className='imageUpload regular' alt='' />
+                <span className="camera">
+                    <video ref={videoRef}></video>
+                    <input type='button' value='Take picture' className="takePicture regular" onClick={takePhoto} />
+                </span>
+                <span className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+                    <canvas ref={photoRef}></canvas>
+                    <input type='button' value='Retake picture' className="takePicture regular" onClick={closePhoto} />
+                </span>
                 <ContinueBtn to='/chooseplan' />
             </form>
         </>
--- src/layouts/Continuebtn_upload.js
+++ src/layouts/Continuebtn_upload.js
@@ -8,7 +8,7 @@ function ContinueBtn_upload(props) {
     return (
         <Link to={props.to}>
             <button className='continue_upload' type="submit" onClick={continueHandler}> Continue <svg xmlns="http://www.w3.org/2000/svg" id='arrow' width="27" height="10" viewBox="0 0 37 20" fill="#1207ab">
-                    <path d="M35.5 10H2M35.5 10L27.5 18.5M35.5 10L27.5 2" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M35.5 10H2M35.5 10L27.5 18.5M35.5 10L27.5 2" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
             </button>
         </Link>
